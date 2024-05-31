7e95011d6695f9e7f8ce571dd25610ac37f90ad5
irinkadat <irina.datoshvili.1@btu.edu.ge>
2024-05-31 11:57:50 +0400

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Irinka Datoshvili on 31.05.24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
